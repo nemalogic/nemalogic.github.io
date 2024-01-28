15baafba3fc02569ef55dcdb6a284195984ccaa8
Lawrence Perepolkin <lawrence.perepolkin@gmail.com>
2024-01-27 23:16:04 -0800

    add

---
+++ ext-js/Fifa_App/build/temp/development/Fifa_app/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Nemalogic_Git_Hub\nemalogic.github.io\ext-js\Fifa_App/build/.sass-cache'
---
+++ ext-js/Fifa_App/build/temp/development/Fifa_app/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Nemalogic_Git_Hub\nemalogic.github.io\ext-js\Fifa_App\app.js
